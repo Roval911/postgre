3a3b8753f7f2dee689e3a4a7b2683bb47beb6c34
slavarodionov <roval911@gmail.com>
2024-11-10 22:03:46 +0300

    createSnippet

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("ЗДаров голова"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Информация о заметке"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(405)
+		w.Write([]byte("запрещенный метод"))
+		return
+	}
+
+	w.Write([]byte("ФОрма для создания новой заметки"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:4000\"")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
