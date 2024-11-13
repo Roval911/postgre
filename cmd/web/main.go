9284703a42f2e1c2b50af4c0b4e10acf3b93dd1a
slavarodionov <roval911@gmail.com>
2024-11-13 23:05:51 +0300

    static

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Запуск сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
